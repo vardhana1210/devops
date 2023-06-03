1bd60bc4d32b2f7930baa68dac80c7134861f65c
vardhana1210 <vardhana1210@gmail.com>
2023-06-03 23:53:55 +0530

    updated tf files

---
+++ eks_infra/main.tf
@@ -0,0 +1,215 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "harsha-eks-demo"
+    key            = "terraform.tfstate"
+    region         = "us-west-1"
+  }
+}
+
+// Create VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+// Create subnets across 3 availability zones
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-west-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_c" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-west-1c"
+  map_public_ip_on_launch = true
+}
+
+// Create route table and associate with subnets
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route" "internet_gateway_route" {
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gateway.id
+}
+
+resource "aws_route_table_association" "subnet_a_association" {
+  subnet_id      = aws_subnet.subnet_a.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet_b_association" {
+  subnet_id      = aws_subnet.subnet_b.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet_c_association" {
+  subnet_id      = aws_subnet.subnet_c.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+// Create security group
+resource "aws_security_group" "security_group" {
+  name        = "eks_security_group"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+// Create EKS cluster
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "eks-demo"
+  role_arn = aws_iam_role.eks_role.arn
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.subnet_a.id,
+      aws_subnet.subnet_b.id,
+      aws_subnet.subnet_c.id
+    ]
+    security_group_ids = [aws_security_group.security_group.id]
+  }
+}
+
+// Create IAM role for EKS
+resource "aws_iam_role" "eks_role" {
+  name = "eks_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+// Create EKS node group
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "ng-default"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [
+    aws_subnet.subnet_a.id,
+    aws_subnet.subnet_b.id,
+    aws_subnet.subnet_c.id
+  ]
+  instance_types = ["m5.xlarge"]
+  scaling_config {
+    desired_size = 3
+    min_size     = 1
+    max_size     = 10
+  }
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks_node_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+// Create Kubernetes namespace
+resource "kubernetes_namespace" "namespace" {
+  metadata {
+    name = "harsha"
+  }
+}
+
+// Enable Istio on namespace
+resource "kubernetes_namespace" "istio_namespace" {
+  metadata {
+    name = "istio-system"
+    labels = {
+      "istio-injection" = "enabled"
+    }
+  }
+}
+
+// Deploy Prometheus
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  namespace  = kubernetes_namespace.namespace.metadata[0].name
+}
+
+// Deploy Grafana
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = kubernetes_namespace.namespace.metadata[0].name
+}
+
+// Deploy logging tool
+resource "helm_release" "logging_tool" {
+  name       = "logging-tool"
+  repository = "https://helm.elastic.co"
+  chart      = "elasticsearch"
+  namespace  = kubernetes_namespace.namespace.metadata[0].name
+}
+
+// Deploy ALB controller
+resource "helm_release" "alb_controller" {
+  name       = "alb-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.3.2"
+  namespace  = kubernetes_namespace.namespace.metadata[0].name
+}
