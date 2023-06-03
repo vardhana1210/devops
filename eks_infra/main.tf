9d2e6fb11ccd6d753a09816b9d83c7d0956aed3d
vardhana1210 <vardhana1210@gmail.com>
2023-06-04 04:39:27 +0530

    updated 7

--- eks_infra/main.tf
+++ eks_infra/main.tf
@@ -28,7 +28,7 @@ resource "aws_subnet" "subnet_b" {
 resource "aws_subnet" "subnet_c" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-west-1a"
+  availability_zone       = "us-west-1b"
   map_public_ip_on_launch = true
 }
 
