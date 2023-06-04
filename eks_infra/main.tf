8ac8bd8c1b284768f79aa08b382d681e4cab7755
vardhana1210 <vardhana1210@gmail.com>
2023-06-04 10:32:15 +0530

    updated2

--- eks_infra/main.tf
+++ eks_infra/main.tf
@@ -181,7 +181,6 @@ data "aws_eks_cluster_auth" "cluster_auth" {
 }
 
 provider "kubernetes" {
-  load_config_file       = false
   host                   = aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
