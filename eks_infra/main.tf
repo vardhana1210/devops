a75fb696fc1844168fcf864682e23db585aa3071
vardhana1210 <vardhana1210@gmail.com>
2023-06-04 10:58:12 +0530

    kube version 1.23

--- eks_infra/main.tf
+++ eks_infra/main.tf
@@ -95,11 +95,12 @@ resource "aws_security_group" "security_group" {
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "eks-demo"
   role_arn = aws_iam_role.eks_role.arn
-  kubernetes_version = "1.23"
+ 
   vpc_config {
     subnet_ids         = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id, aws_subnet.subnet_c.id]
     security_group_ids = [aws_security_group.security_group.id]
   }
+  kubernetes_version = "1.23"
 }
 
 resource "aws_iam_role" "eks_role" {
