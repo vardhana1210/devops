6351e358e82022709298512d8be698a21bfe2190
vardhana1210 <vardhana1210@gmail.com>
2023-06-04 11:39:24 +0530

    remove eks to 1.23

--- eks_infra/main.tf
+++ eks_infra/main.tf
@@ -95,7 +95,7 @@ resource "aws_security_group" "security_group" {
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "eks-demo"
   role_arn = aws_iam_role.eks_role.arn
-  version = "1.23"
+  
  
   vpc_config {
     subnet_ids         = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id, aws_subnet.subnet_c.id]
