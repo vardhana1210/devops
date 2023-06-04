41997da81bcff00d427ecf5c6390749995658101
vardhana1210 <vardhana1210@gmail.com>
2023-06-04 09:55:06 +0530

    update provider 6

--- eks_infra/main.tf
+++ eks_infra/main.tf
@@ -184,7 +184,6 @@ provider "kubernetes" {
   host                   = "${aws_eks_cluster.eks_cluster.endpoint}"
   cluster_ca_certificate = "${base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)}"
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-  load_config_file       = false
   version                = "~> 2.3"
 
   exec {
