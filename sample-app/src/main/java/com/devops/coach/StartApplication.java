66f1bfdbd2c557db24311841e4e999a31776ca30
vardhana1210 <vardhana1210@gmail.com>
2023-06-04 19:32:32 +0530

    updated folder path

---
+++ sample-app/src/main/java/com/devops/coach/StartApplication.java
@@ -0,0 +1,24 @@
+package com.devops.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class StartApplication {
+
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "Howdy Folks..Welcome to demoapp deployment!");
+        model.addAttribute("msg", "We are deploying springboot app inside EKS cluster using helm and Jenkins pipeline!!!!!");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
