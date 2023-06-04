52ebf00d0b4da4c3a2db34880f72034e87515ec8
vardhana1210 <vardhana1210@gmail.com>
2023-06-04 10:30:43 +0530

    updated1

--- eks_infra/main.tf
+++ eks_infra/main.tf
@@ -181,10 +181,11 @@ data "aws_eks_cluster_auth" "cluster_auth" {
 }
 
 provider "kubernetes" {
-  host                   = "${aws_eks_cluster.eks_cluster.endpoint}"
-  cluster_ca_certificate = "${base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)}"
+  load_config_file       = false
+  host                   = aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-  version                = "~> 2.3"
+  config_path            = abspath(path.module) # Use the absolute path to the module directory
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
