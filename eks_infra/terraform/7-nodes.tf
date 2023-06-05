6b083ad9528753580b0cd369306d95518580bb42
vardhana1210 <vardhana1210@gmail.com>
2023-06-05 23:57:12 +0530

    updated loki helm

--- eks_infra/terraform/7-nodes.tf
+++ eks_infra/terraform/7-nodes.tf
@@ -59,6 +59,17 @@ resource "aws_eks_node_group" "private-nodes" {
 data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.demo.name
 }
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
 resource "kubernetes_namespace" "harsha_namespace" {
   metadata {
     name = "logging"
